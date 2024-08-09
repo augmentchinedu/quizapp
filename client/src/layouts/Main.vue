cdce96aed75e04091aa8bc633f4c2d0c89a14cca
augmentchinedu <augmentchinedu@gmail.com>
2024-08-09 14:43:07 +0000

    Scoll Behavior Fixed

--- client/src/layouts/Main.vue
+++ client/src/layouts/Main.vue
@@ -1,21 +1,19 @@
 <template>
-  <v-responsive class="border rounded">
-    <v-app class="h-screen">
-      <v-app-bar :elevation="2" color="#4A148C">
-        <template v-slot:prepend>
-          <v-avatar color="#FFFFFF">QA</v-avatar>
-        </template>
+  <v-app class="h-screen border rounded">
+    <v-app-bar :elevation="2" color="#4A148C">
+      <template v-slot:prepend>
+        <v-avatar color="#FFFFFF">QA</v-avatar>
+      </template>
 
-        <v-app-bar-title>{{ route.name }}</v-app-bar-title>
-      </v-app-bar>
+      <v-app-bar-title>{{ route.name }}</v-app-bar-title>
+    </v-app-bar>
 
-      <v-main>
-        <v-container class="h-full">
-          <router-view></router-view>
-        </v-container>
-      </v-main>
-    </v-app>
-  </v-responsive>
+    <v-main>
+      <v-container class="h-full">
+        <router-view></router-view>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script setup>
